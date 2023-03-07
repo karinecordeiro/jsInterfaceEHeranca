0fc93f7153cdcf0f8ab5af69ebc83a3ba6e10116
Karine C <karine.cordeiro1998@hotmail.com>
2023-03-07 12:39:12 -0300

    implementando herança entre classes

--- Conta.js
+++ Conta.js
@@ -6,12 +6,31 @@ export class Conta{
         this._agencia = agencia;
     }
 
-    sacar(valor) {
-        taxa = 1.1 * valor;
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
         }
+    }
+    
+    get cliente(){
+        return this._cliente;
+    }
+    get saldo(){
+        return this._saldo;
+    } 
+
+    sacar(valor) {
+        let taxa = 1
+        return this._sacar(valor, taxa);
+    }
+
+    _sacar(valor, taxa){
+        const valorSacado = taxa * valor;
+        if (this._saldo >= valorSacado) {
+            this._saldo -= valorSacado;
+            return valorSacado;
+        }
+        return 0;
     }
 
     depositar(valor) {
@@ -22,9 +41,7 @@ export class Conta{
     }
 
     tranferir(valor, conta) {
-
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
-
     }
 }
--- index.js
+++ index.js
@@ -7,13 +7,14 @@ import {Conta} from "./Conta.js"
 
 const cliente1 = new Cliente("Cassia", 77788899900);
 
-const conta1 = new Conta(0,1001, cliente1);
+const conta1 = new ContaCorrente (cliente1, 1001);
 conta1.depositar(800);
 conta1.sacar(150);
 
-const contaPoupanca = new Conta(420, cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(420, cliente1, 1001);
+contaPoupanca.sacar(5);''
 
-console.log(contaPoupanca);
 console.log(conta1);
+console.log(contaPoupanca);
 
-////
+////'
